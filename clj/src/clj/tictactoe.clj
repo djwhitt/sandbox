121aef1c080e5ebd0942544b595658e2f8327c48
David Whittington <djwhitt@gmail.com>
2011-12-05 22:33:39 -0500

    Better tic-tac-toe representation (I think)

--- clj/src/clj/tictactoe.clj
+++ clj/src/clj/tictactoe.clj
@@ -2,33 +2,31 @@
   (:refer-clojure :exclude [==])
   (:use [clojure.core.logic]))
 
-(defne rowo [b w]
-  ([[w w w . ?r] _])
-  ([[_ _ _ . ?r] _] (rowo ?r w)))
+(defrel left-of Position Position)
 
-(defne colo [b w]
-  ([[w _ _ w _ _ w . ?r] _])
-  ([[_ . ?r] _] (colo ?r w)))
+(facts left-of [[[:a 1] [:a 2]]
+                [[:a 2] [:a 3]]
+                [[:b 1] [:b 2]]
+                [[:b 2] [:b 3]]
+                [[:c 1] [:c 2]]
+                [[:c 2] [:c 3]]])
+
+(defn rowo [b w]
+  (fresh [p1 p2 p3]
+    (left-of p1 p2)
+    (left-of p2 p3)
+    (project [b p1 p2 p3]
+      (!= w nil)
+      (== w (b p1))
+      (== w (b p2))
+      (== w (b p3)))))
 
 (comment
   (run* [q]
-    (fresh [g]
-      (== '(e x e
-            e x e
-            x x e) g)
-      (colo g q)))
-
-  (run* [q]
-    (fresh [g]
-      (== '(e x e
-            e x e
-            x e x) g)
-      (colo g q)))
-
-  (run* [q]
-    (fresh [g]
-      (== '(e x e
-            e x e
-            x x x) g)
-      (rowo g q)))
+    (fresh [b w]
+      (== {[:a 1] :x, [:a 2] :e, [:a 3] :e
+           [:b 1] :o, [:b 2] :o, [:b 3] :o
+           [:c 1] :x, [:c 2] :x, [:c 3] :x} b)
+      (rowo b w)
+      (== w q)))
   )
