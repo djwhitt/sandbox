7cef83a0a107a6fa35372d20385b8196a5b98f42
David Whittington <djwhitt@gmail.com>
2011-12-13 14:15:40 -0500

    Macro for boardo and representation tweak

--- clj/src/clj/tictactoe.clj
+++ clj/src/clj/tictactoe.clj
@@ -2,14 +2,26 @@
   (:refer-clojure :exclude [==])
   (:use [clojure.core.logic]))
 
+(defrel coordinate Position Coordinate)
+
 (defrel left-of Position Position)
 
-(facts left-of [[[:a 1] [:a 2]]
-                [[:a 2] [:a 3]]
-                [[:b 1] [:b 2]]
-                [[:b 2] [:b 3]]
-                [[:c 1] [:c 2]]
-                [[:c 2] [:c 3]]])
+(facts coordinate [[0 [0 0]]
+                   [1 [1 0]]
+                   [2 [2 0]]
+                   [3 [0 1]]
+                   [4 [1 1]]
+                   [5 [2 1]]
+                   [6 [0 2]]
+                   [7 [1 2]]
+                   [8 [2 2]]])
+
+(facts left-of [[0 1] 
+                [1 2]
+                [3 4]
+                [4 5]
+                [6 7]
+                [7 8]])
 
 (defn pieceo [x]
   (conde
@@ -17,18 +29,11 @@
    ((== x 'x))
    ((== x 'o))))
 
-(defn boardo [b]
-  (fresh [a3 b3 c3 a2 b2 c2 a1 b1 c1]
-    (== b [a3 b3 c3 a2 b2 c2 a1 b1 c1])
-    (pieceo a3)
-    (pieceo b3)
-    (pieceo c3)
-    (pieceo a2)
-    (pieceo b2)
-    (pieceo c2)
-    (pieceo a1)
-    (pieceo b1)
-    (pieceo c1)))
+(defmacro boardo [b]
+  (let [ps ['p0 'p1 'p2 'p3 'p4 'p5 'p6 'p7 'p8]]
+    `(fresh ~ps
+       (== ~b ~ps)
+       ~@(map (fn [p] (list 'pieceo p)) ps))))
 
 (defn rowo [b w]
   (fresh [p1 p2 p3]
@@ -40,16 +45,12 @@
       (== w (b p2))
       (== w (b p3)))))
 
-
-
-
-
 (comment
   (run* [q]
     (fresh [b w]
-      (== {[:a 1] :x, [:a 2] :e, [:a 3] :e
-           [:b 1] :o, [:b 2] :o, [:b 3] :o
-           [:c 1] :x, [:c 2] :x, [:c 3] :x} b)
+      (== [:x :e :e
+           :o :o :o
+           :x :x :x] b)
       (rowo b w)
       (== w q)))
 
@@ -61,11 +62,4 @@
         cols   ['a 'b 'c]
         coords (for [x rows y cols] [y x])]
     coords)
-
-  (run* [q]
-    (fresh [a]
-      (== a {:a 1 :b 2})
-      (== a {:a q :b 2}))
-    )
-
   )
