4deebaf82d61f7d50c904d1731c8db6d3a130a0a
David Whittington <djwhitt@gmail.com>
2011-12-14 22:18:29 -0500

    Adding relations for rows and columns to tic-tac-toe game

--- clj/src/clj/tictactoe.clj
+++ clj/src/clj/tictactoe.clj
@@ -3,8 +3,8 @@
   (:use [clojure.core.logic]))
 
 (defrel coordinate Position Coordinate)
-
 (defrel left-of Position Position)
+(defrel above Position Position)
 
 (facts coordinate [[0 [0 0]]
                    [1 [1 0]]
@@ -23,6 +23,13 @@
                 [6 7]
                 [7 8]])
 
+(facts above [[0 3] 
+              [1 4]
+              [2 5]
+              [3 6]
+              [4 7]
+              [5 8]])
+
 (defn pieceo [x]
   (conde
    ((== x '_))
@@ -35,24 +42,36 @@
        (== ~b ~ps)
        ~@(map (fn [p] (list 'pieceo p)) ps))))
 
-(defn rowo [b w]
+(defn rowo [b r]
   (fresh [p1 p2 p3]
     (left-of p1 p2)
     (left-of p2 p3)
     (project [b p1 p2 p3]
-      (!= w nil)
-      (== w (b p1))
-      (== w (b p2))
-      (== w (b p3)))))
+      (== r [(b p1) (b p2) (b p3)]))))
+
+(defn colo [b c]
+  (fresh [p1 p2 p3]
+    (above p1 p2)
+    (above p2 p3)
+    (project [b p1 p2 p3]
+      (== c [(b p1) (b p2) (b p3)]))))
 
 (comment
   (run* [q]
-    (fresh [b w]
+    (fresh [b r]
       (== [:x :e :e
            :o :o :o
            :x :x :x] b)
-      (rowo b w)
-      (== w q)))
+      (rowo b r)
+      (== q r)))
+
+  (run* [q]
+    (fresh [b c]
+      (== [:x :e :e
+           :o :o :o
+           :x :x :x] b)
+      (colo b c)
+      (== q c)))
 
   (run 12 [q]
     (boardo q))
