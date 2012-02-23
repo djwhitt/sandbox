2407976964a1179d1c58d3c92b6d2592c6917c68
David Whittington <djwhitt@gmail.com>
2012-02-23 18:45:39 -0500

    Updating version of core.logic

--- clj/project.clj
+++ clj/project.clj
@@ -1,6 +1,6 @@
 (defproject clj "0.0.1-SNAPSHOT"
   :description "Misc Clojure experiments"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.logic "0.6.5"]
+                 [org.clojure/core.logic "0.6.7"]
                  [clj-redis "0.0.13-SNAPSHOT"]
                  [incanter "1.3.0-SNAPSHOT"]])
