85815e813f1ad97dbf6f524d2507514e7d2f9d67
David Whittington <djwhitt@gmail.com>
2012-02-23 21:36:14 -0500

    Adding stuff for my core.logic presentation

--- clj/src/clj/logic.clj
+++ clj/src/clj/logic.clj
@@ -28,13 +28,74 @@
 
 (defn parent [p child]
   (conde
-   ((father p child))
-   ((mother p child))))
+   [(father p child)]
+   [(mother p child)]))
 
 (comment
+  
   (run* [r]
     (fresh [x y]
       (resto '(grape raisin pair) x)
       (firsto '((a) (b) (c)) y)
       (== (lcons x y) r)))
+
+
+  ;; unification
+  (run* [q]
+    (== q 1))
+
+  ;; fresh
+  (run* [q]
+    (fresh [a]
+      (== q a)))
+
+  (run* [q]
+    (fresh [a]
+      (== a 1)
+      (== q a)))
+
+  ;; conde
+  (run* [q]
+    (conde
+     [(== q 1)]
+     [(== q 2)]))
+
+  (run* [q]
+    (fresh [a]
+      (conde
+       [(== a 1) (== q a)]
+       [(== a 2) (== q a)]
+       [(== 1 1) (== q a)]
+       [(== 1 2) (== q a)])))
+
+  ;; examples using appendo
+  (run* [q]
+    (fresh [a b]
+      (== a '(1 2 3))
+      (== b '(4 5 6))
+      (appendo a b q )))
+
+  (run* [q]
+    (fresh [a b]
+      (== a '(1 2 3))
+      (== b '(4 5 6))
+      (appendo a q b)))
+
+  (run* [q]
+    (fresh [a b c d e]
+      (== a '(1 2 3))
+      (== e '(1 2 3 4 5 6))
+      (appendo a b c)
+      (appendo c d e)
+      (== q [b d])))
+
+  (defn sublisto
+    [x y]
+    (fresh [a b c]
+      (appendo a b y)
+      (appendo x c b)))
+
+  (run 5 [q]
+    (sublisto q [1 2 3 4 5]))
+  
   )
