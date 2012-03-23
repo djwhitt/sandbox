bff08064823e780235bd6ed6b371d8855ebb5e8d
David Whittington <djwhitt@gmail.com>
2012-03-22 21:36:21 -0400

    Beautify nil rule slightly

--- clojurerb/lib/clojurerb/reader.rb
+++ clojurerb/lib/clojurerb/reader.rb
@@ -3,8 +3,8 @@ require 'parslet'
 module ClojureRB
   class Reader < Parslet::Parser
     rule(:integer) { match['0-9'].repeat(1).as(:integer) }
-    rule(:_nil) { str('nil').as(:nil) }
-    rule(:form) { integer | _nil }
+    rule(:nil) { str('nil').as(:nil) }
+    rule(:form) { self.nil | integer }
     
     root(:form)
   end
