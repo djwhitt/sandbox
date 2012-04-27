f0f8dd1227b03d5c8f47299f6373c85f6ee19c96
David Whittington <djwhitt@gmail.com>
2012-04-26 23:19:52 -0400

    Messing around with seesaw

--- clj/project.clj
+++ clj/project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.logic "0.6.7"]
                  [clj-redis "0.0.13-SNAPSHOT"]
-                 [incanter "1.3.0-SNAPSHOT"]])
+                 [incanter "1.3.0-SNAPSHOT"]
+                 [seesaw "1.4.0"]])
