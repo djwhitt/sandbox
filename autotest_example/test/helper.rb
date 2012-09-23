d7455902249d42ebe9d9b27d7d3616b00f00b434
David Whittington <djwhitt@gmail.com>
2012-09-22 20:59:43 -0400

    Removing autotest_example

--- autotest_example/test/helper.rb
+++
@@ -1,3 +0,0 @@
-require 'rubygems'
-gem 'minitest'
-require 'minitest/autorun'
--- autotest_example/test/test_foo.rb
+++
@@ -1,8 +0,0 @@
-require 'foo'
-
-class TestFoo < MiniTest::Unit::TestCase
-  def test_bar
-    f = Foo.new
-    assert_equal 'bar', f.bar
-  end
-end
