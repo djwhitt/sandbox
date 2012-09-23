2d13c83ed772a8ed9fdfcb7f54e5723d512c4bb1
David Whittington <djwhitt@gmail.com>
2012-09-22 20:58:54 -0400

    Removing clojurerb

--- clojurerb/test/helper.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'isolate/now'
-gem 'minitest'
-require 'minitest/autorun'
-require 'minitest/mock'
-
-require 'clojurerb'
-
-$:.unshift File.dirname(__FILE__) + '/../lib'
